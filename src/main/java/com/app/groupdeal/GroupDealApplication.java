ec7f653cf27d909b351644cf22c95c3af0baa19f
geuning <thethe133@naver.com>
2025-10-27 00:33:05 +0900

    initial commit

---
+++ src/main/java/com/app/groupdeal/GroupDealApplication.java
@@ -0,0 +1,13 @@
+package com.app.groupdeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupDealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupDealApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/groupdeal/GroupDealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.groupdeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupDealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
