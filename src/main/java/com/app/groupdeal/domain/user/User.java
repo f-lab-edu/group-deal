3e2fe43dffd9053b3a61845da6d5aafb4f69950e
Junggeun Kim <thethe133@naver.com>
2025-10-31 15:43:21 +0900

    feat: 도메인 생성

--- src/main/java/com/app/groupdeal/domain/user/User.java
+++ src/main/java/com/app/groupdeal/domain/user/User.java
@@ -1,4 +1,42 @@
 package com.app.groupdeal.domain.user;
 
-public class User {
+import com.app.groupdeal.domain.common.BaseDomain;
+import com.app.groupdeal.infrastructure.user.UserEntity;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class User extends BaseDomain {
+
+    private Long userId;
+    private String email;
+    private String password;
+    private String nickname;
+
+    @Builder
+    public User(Long userId, String email, String password, String nickname) {
+        this.userId = userId;
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+
+    }
+
+    public void encryptPassword(String encryptPassword) {
+        this.password = encryptPassword;
+    }
+
+    public boolean isValidEmail(){
+        return email != null && !email.contains("@");
+    }
+
+
+    public UserEntity toEntity() {
+        return UserEntity.builder()
+                .userId(userId)
+                .email(email)
+                .password(password)
+                .nickname(nickname)
+                .build();
+    }
 }
