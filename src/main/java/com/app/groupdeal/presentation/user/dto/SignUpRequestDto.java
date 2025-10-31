c9db792eb7ceeae6b9b190d1563be1e83d947742
Junggeun Kim <thethe133@naver.com>
2025-10-31 15:46:06 +0900

    refactor: 객체 변환 메서드 추가

--- src/main/java/com/app/groupdeal/presentation/user/dto/SignUpRequestDto.java
+++ src/main/java/com/app/groupdeal/presentation/user/dto/SignUpRequestDto.java
@@ -1,5 +1,6 @@
 package com.app.groupdeal.presentation.user.dto;
 
+import com.app.groupdeal.domain.user.User;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -21,4 +22,14 @@ public class SignUpRequestDto {
 
     @NotBlank(message = "닉네임은 필수 입력값입니다.")
     private String nickname;
+
+    public User toDomain(){
+        return User.builder()
+                .email(email)
+                .nickname(nickname)
+                .password(password)
+                .build();
+    }
+
+
 }
--- src/main/java/com/app/groupdeal/presentation/user/dto/SignUpResponseDto.java
+++ src/main/java/com/app/groupdeal/presentation/user/dto/SignUpResponseDto.java
@@ -1,5 +1,6 @@
 package com.app.groupdeal.presentation.user.dto;
 
+import com.app.groupdeal.domain.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,4 +15,12 @@ public class SignUpResponseDto {
     private Long userId;
     private String email;
     private String nickname;
+
+    public static SignUpResponseDto of(User user) {
+        return SignUpResponseDto.builder()
+                .userId(user.getUserId())
+                .email(user.getEmail())
+                .nickname(user.getNickname())
+                .build();
+    }
 }
