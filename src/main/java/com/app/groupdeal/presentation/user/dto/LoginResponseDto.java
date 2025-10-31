e2f4740b198f68b4b73c5835da62efb9a841a2ae
geuning <thethe133@naver.com>
2025-10-31 22:52:43 +0900

    feat: 로그인 관련 dto추가

--- src/main/java/com/app/groupdeal/presentation/user/dto/LoginResponseDto.java
+++ src/main/java/com/app/groupdeal/presentation/user/dto/LoginResponseDto.java
@@ -1,4 +1,26 @@
 package com.app.groupdeal.presentation.user.dto;
 
+import com.app.groupdeal.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class LoginResponseDto {
+
+    private Long userId;
+    private String email;
+    private String nickname;
+
+    public static LoginResponseDto of(User user) {
+        return LoginResponseDto.builder()
+                .userId(user.getUserId())
+                .email(user.getEmail())
+                .nickname(user.getNickname())
+                .build();
+    }
 }
