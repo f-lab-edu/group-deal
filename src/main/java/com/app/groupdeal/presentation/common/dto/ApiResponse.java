a62e7c957173bc5c998e592b52ea049158e33197
geuning <thethe133@naver.com>
2025-10-31 23:26:37 +0900

    feat: data 없는 공통 response 반환 메서드 추가

--- src/main/java/com/app/groupdeal/presentation/common/dto/ApiResponse.java
+++ src/main/java/com/app/groupdeal/presentation/common/dto/ApiResponse.java
@@ -1,15 +1,26 @@
 package com.app.groupdeal.presentation.common.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse<T> {
-    private boolean success;
-    private T data;
+    private final boolean success;
+    private final T data;
+
+    public ApiResponse(boolean success) {
+        this.success = success;
+        this.data = null;
+    }
 
     public static <T> ApiResponse<T> success(T data) {
         return new ApiResponse<>(true, data);
+    }
+
+    public static <T> ApiResponse<T> success() {
+        return new ApiResponse<>(true);
     }
 }
