1c9eea69020ffe13e1048f24340132c09ea12c70
Junggeun Kim <thethe133@naver.com>
2025-11-06 12:11:41 +0900

    refactor: jpa 설정 통합

--- src/main/java/com/app/groupdeal/global/config/jpa/JpaConfig.java
+++ src/main/java/com/app/groupdeal/global/config/jpa/JpaConfig.java
@@ -5,9 +5,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableJpaAuditing
+@EnableJpaRepositories(
+        basePackages = "com.app.groupdeal.infrastructure",
+        considerNestedRepositories = true
+)
 public class JpaConfig {
 
     @Bean
