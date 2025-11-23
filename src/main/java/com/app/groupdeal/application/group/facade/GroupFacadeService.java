5f91cd53cfc503fe8a01b908b34b53f7d1d39b5e
geuning <thethe133@naver.com>
2025-11-23 15:02:56 +0900

    feat: facade 서비스 추가

--- src/main/java/com/app/groupdeal/application/group/facade/GroupFacadeService.java
+++ src/main/java/com/app/groupdeal/application/group/facade/GroupFacadeService.java
@@ -5,15 +5,15 @@ import com.app.groupdeal.application.group.service.GroupService;
 import com.app.groupdeal.application.user.service.UserService;
 import com.app.groupdeal.domain.group.model.Group;
 import com.app.groupdeal.domain.group.model.GroupMember;
+import com.app.groupdeal.global.error.ErrorType;
+import com.app.groupdeal.global.error.exception.BusinessException;
 import com.app.groupdeal.presentation.common.dto.PageResponse;
-import com.app.groupdeal.presentation.group.dto.CreateGroupRequestDto;
-import com.app.groupdeal.presentation.group.dto.CreateGroupResponseDto;
-import com.app.groupdeal.presentation.group.dto.DetailGroupResponseDto;
-import com.app.groupdeal.presentation.group.dto.SearchGroupResponseDto;
+import com.app.groupdeal.presentation.group.dto.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import com.app.groupdeal.domain.user.User;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -57,5 +57,29 @@ public class GroupFacadeService {
         return DetailGroupResponseDto.of(group, groupMembers);
 
 
+    }
+
+    @Transactional
+    public JoinGroupResponseDto joinGroup(Long groupId, Long userId, String nickname) {
+
+        Group group = groupService.findById(groupId);
+
+        if(group.getHostMemberId().equals(userId)) {
+            throw new BusinessException(ErrorType.CANNOT_JOIN_OWN_GROUP);
+        }
+
+        group.validateJoinable();
+
+        if (groupMemberService.isAlreadyJoined(groupId, userId)) {
+            throw new BusinessException(ErrorType.ALREADY_JOINED);
+        }
+
+        GroupMember updatedGroupMember = groupMemberService.joinGroup(groupId, userId, nickname);
+
+        groupService.increaseParticipant(groupId);
+
+        Group updatedGroup = groupService.findById(groupId);
+
+        return JoinGroupResponseDto.of(updatedGroup, updatedGroupMember);
     }
 }
