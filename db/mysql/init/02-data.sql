5f5e3faf7cd655b0b59c6a953c63bb484686cdd8
geuning <thethe133@naver.com>
2025-12-13 02:05:57 +0900

    feat: 더미 데이터 추가

--- db/mysql/init/02-data.sql
+++ db/mysql/init/02-data.sql
@@ -1,13 +1,16 @@
+SET NAMES utf8mb4;
+SET CHARACTER SET utf8mb4;
+
 INSERT INTO users (email, password, nickname, created_time, updated_time, created_by, updated_by)
 VALUES ('host1@test.com', '1234', '호스트1', NOW(), NOW(), 'system', 'system'),
        ('test1@test.com', '1234', '테스트1', NOW(), NOW(), 'system', 'system');
 
 INSERT INTO `groups` (product_name, category, description, original_price, target_participants, divided_unit, recruitment_minutes, deadline_at, meeting_location, meeting_at, host_member_id, host_member_name, status, current_participants, created_time, updated_time, created_by, updated_by)
-VALUES ('코스트코 견과류 믹스 3kg', '식품', '신선한 견과류입니다.', 30000, 3, '500g씩', 1440, NOW() + INTERVAL 1 DAY, '강남역 3번 출구', '2026-01-22 14:00:00', 1, '호스트1', 'RECRUITING', 2, NOW(), NOW(), 1, 2);
+VALUES ('코스트코 견과류 믹스 3kg', '식품', '신선한 견과류입니다.', 30000, 3, '500g씩', 1440, NOW() + INTERVAL 1 DAY, '강남역 3번 출구', NOW() + INTERVAL 2 DAY, 1, '호스트1', 'RECRUITING', 2, NOW(), NOW(), 'system', 'system');
 
 INSERT INTO group_members (group_id, user_id, nickname, group_member_type, group_member_status, joined_at, created_time, updated_time, created_by, updated_by)
-VALUES (1, 1, '호스트1', 'HOST', 'JOINED', NOW(), NOW(), NOW(), 1, 1),
-       (1, 2, '테스트1', 'MEMBER', 'JOINED', NOW(), NOW(), NOW(), 2, 2);
+VALUES (1, 1, '호스트1', 'HOST', 'JOINED', NOW(), NOW(), NOW(), 'system', 'system'),
+       (1, 2, '테스트1', 'MEMBER', 'JOINED', NOW(), NOW(), NOW(), 'system', 'system');
 
 DELIMITER $$
 
