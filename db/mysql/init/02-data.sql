7c1669efdc9f90fe0aa3e2f9f4986fafa3a75bd9
geuning <thethe133@naver.com>
2025-12-28 20:07:34 +0900

    refactor: script 수정

--- db/mysql/init/02-data.sql
+++ db/mysql/init/02-data.sql
@@ -6,7 +6,7 @@ VALUES ('host1@test.com', '$2a$10$FAkvDPjLjmwJhd9o6YDStePThk5E1ZKHNNFFcjKqSFj.Vz
        ('member1@test.com', '$2a$10$FAkvDPjLjmwJhd9o6YDStePThk5E1ZKHNNFFcjKqSFj.VzF6qbWQa', '멤버1', NOW(), NOW(), 'system', 'system');
 
 INSERT INTO `groups` (product_name, category, description, original_price, target_participants, divided_unit, recruitment_minutes, deadline_at, meeting_location, meeting_at, host_member_id, host_member_name, status, current_participants, created_time, updated_time, created_by, updated_by)
-VALUES ('코스트코 견과류 믹스 3kg', '식품', '신선한 견과류입니다.', 30000, 3, '500g씩', 1440, NOW() + INTERVAL 1 DAY, '강남역 3번 출구', NOW() + INTERVAL 2 DAY, 1, '호스트1', 'RECRUITING', 2, NOW(), NOW(), 'system', 'system');
+VALUES ('코스트코 견과류 믹스 3kg', '식품', '신선한 견과류입니다.', 30000, 300, '500g씩', 1440, NOW() + INTERVAL 1 DAY, '강남역 3번 출구', NOW() + INTERVAL 2 DAY, 1, '호스트1', 'RECRUITING', 200, NOW(), NOW(), 'system', 'system');
 
 INSERT INTO group_members (group_id, user_id, nickname, group_member_type, group_member_status, joined_at, created_time, updated_time, created_by, updated_by)
 VALUES (1, 1, '호스트1', 'HOST', 'JOINED', NOW(), NOW(), NOW(), 'system', 'system'),
@@ -18,7 +18,7 @@ DROP PROCEDURE IF EXISTS generate_dummy_users$$
 CREATE PROCEDURE generate_dummy_users()
 BEGIN
   DECLARE i INT DEFAULT 1;
-  WHILE i <= 1000 DO
+  WHILE i <= 20000 DO
     INSERT INTO users (email, password, nickname, created_time, updated_time, created_by, updated_by)
     VALUES (
       CONCAT('test', i, '@test.com'),
